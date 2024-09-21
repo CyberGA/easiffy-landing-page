154086f7e976a31aaef5a08263b7896e6d75c795
Gbenga <52829434+CyberGA@users.noreply.github.com>
2024-09-21 08:12:55 +0000

    feat: banner

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -1,12 +1,17 @@
-export default function Banner() {
+import axios from "@/lib/axios";
+
+export default async function Banner() {
+  const res = await axios.post("/press/frontend_text").;
+
+  if (res.status !== 200) {
+    return <></>;
+  } 
   return (
     <section className="py-16 bg-white">
       <div className="overflow-hidden relative">
         <div className="flex animate-scrolling-slide">
           <h1 className="text-5xl sm:text-7xl text-secondary-gray-80/50 whitespace-nowrap font-semibold">
-            Quality Product and Service Quality Product and Service Quality
-            Product and Service Quality Product and Service Quality Product and
-            Service Quality Product and Service
+            { res.data?.content}
           </h1>
         </div>
       </div>
